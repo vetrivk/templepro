76d135029e89b7a96c82babdee498b60d8da079b
vetrivk <vetrivvs07@gmail.com>
2024-01-26 18:43:31 +0530

    add video file

--- script.js
+++ script.js
@@ -2,3 +2,23 @@
 function goToPage(page) {
   window.location.href = page; // This goes back one page in the history
 }
+
+// video play and close
+
+function playVideo() {
+  var video = document.getElementById("myVideo"); // Use the actual ID of your video element
+  var clip = document.querySelector(".clip");
+
+  video.play();
+  clip.classList.add("active");
+}
+
+function closeVideo() {
+  var video = document.getElementById("myVideo"); // Use the actual ID of your video element
+  var clip = document.querySelector(".clip");
+
+  video.pause();
+  clip.classList.remove("active");
+}
+
+// end
